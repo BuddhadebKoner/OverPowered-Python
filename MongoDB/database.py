a1d14e6455d5cf3b1b0bd4a22212aa878200f4a5
buddhadebkoner <jeetkoner36@gmail.com>
2024-05-11 09:41:30 +0530

    chore: Update .gitignore and MongoDB database.py

--- MongoDB/database.py
+++ MongoDB/database.py
@@ -1,7 +1,7 @@
 from pymongo import MongoClient
 from bson import ObjectId
 
-client = MongoClient("mongodb+srv://admin:admin@cluster0.ncvgoge.mongodb.net/", tlsAllowInvalidCertificates = True)
+client = MongoClient("mongodb+srv://admin:admin@cluster0.ncvgoge.mongodb.net/", tlsAllowInvalidCertificates=True)
 # not a good idea 
 
 
@@ -15,17 +15,20 @@ print(video_collection)
 def add_video(name, time):
     video_collection.insert_one({
         "name": name, "time": time
-        })
+    })
+
 
 def list_videos():
     for video in video_collection.find():
         print(f"videos are : {video['_id']} , name {video['name']} and Time {video['time']}")
 
+
 def update_video(video_id, new_name, new_time):
-   video_collection.update_one(
-       {'_id': ObjectId(video_id)},
-       {"$set": {"name": new_name, "time": new_time}}
+    video_collection.update_one(
+        {'_id': ObjectId(video_id)},
+        {"$set": {"name": new_name, "time": new_time}}
     )
+
 
 def delete_video(video_id):
     video_collection.delete_one({"_id": ObjectId(video_id)})
@@ -59,6 +62,7 @@ def main():
             break
         else:
             print("Invalid choice")
+
 
 if __name__ == "__main__":
     main()
---
+++ example.md
@@ -0,0 +1,3 @@
+Recent Contributions:
+
+![Alt](https://repobeats.axiom.co/api/embed/89be0a1a1c8f641c54f9234a7423e7755352c746.svg 'Repobeats analytics image')
